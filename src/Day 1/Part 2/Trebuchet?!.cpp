40b0cfabb37f14d5f9944096a8d42c219aa8a0c8
jeebuscrossaint <thediamond270@gmail.com>
2023-12-01 10:06:29 -0500

    Day 1 Part 2 complete

--- src/Day 1/Part 2/Trebuchet?!.cpp
+++ src/Day 1/Part 2/Trebuchet?!.cpp
@@ -1,31 +1,38 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main() {
-    std::ifstream file("input.txt");
-    if (!file) {
-        std::cerr << "Unable to open file input.txt";
-        return 1;
-    }
+int main()
+{
+    ifstream file("input.txt");
+    vector<int> coord2;
+    unordered_map<string, string> num_map = {{"1","one"}, {"2","two"}, {"3","three"}, {"4","four"}, {"5","five"}, {"6","six"}, {"7","seven"}, {"8","eight"}, {"9","nine"}};
 
-    int totalSum = 0;
-    std::string line;
-    while (std::getline(file, line)) {
-        line.erase(std::remove_if(line.begin(), line.end(), [](char c) { return !std::isdigit(c); }), line.end());
-        int num;
-        if (line.length() == 1) {
-            num = std::stoi(line + line);
-        } else {
-            num = std::stoi(std::string() + line.front() + line.back());
+    for (string line; file >> line;)
+    {
+        unordered_map<size_t, pair<string, string>> finds;
+        for (const auto& num: num_map)
+        {
+            size_t found = line.find(num.second);
+            while (found != string::npos)
+            {
+                finds[found] = num;
+                found = line.find(num.second, found+1);
+            }
         }
-        totalSum += num;
+        vector<int> find_idxs;
+        transform(finds.begin(), finds.end(), back_inserter(find_idxs), [](const auto& pair) { return pair.first; });
+        sort(find_idxs.begin(), find_idxs.end());
+        if (!find_idxs.empty())
+        {
+            line.replace(find_idxs.front(), 1, finds[find_idxs.front()].first);
+            line.replace(find_idxs.back(), 1, finds[find_idxs.back()].first);
+        }
+
+        line.erase(remove_if(line.begin(), line.end(), ::isalpha), line.end());
+        string n = {line.front(), line.back()};
+        coord2.push_back(stoi(n));
     }
-
-    file.close();
-
-    std::cout << "Total sum: " << totalSum << std::endl;
-
-    return 0;
+    auto res2 = accumulate(coord2.begin(), coord2.end(), 0);
+    cout << res2 << endl;
+    return(0);
 }
